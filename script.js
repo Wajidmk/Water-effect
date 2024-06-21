5ffbebab4df68e84435c48c3c4cc4cca6fdb6a99
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-06-21 21:46:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    VANTA.WAVES({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x0077ff, // Wave color
+        shininess: 50.00, // Wave shininess
+        waveHeight: 20.00, // Wave height
+        waveSpeed: 1.00, // Wave speed
+        zoom: 1.00
+    });
+});
